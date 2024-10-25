6ed11867133dadb9e44fe0f5b13f95e748639751
Thomas Waldmann <tw@waldmann-edv.de>
2024-10-25 21:59:00 +0200

    try without package

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 ext_modules = [
-    Extension("borghash._borghash", ["src/borghash/_borghash.pyx"]),
+    Extension("borghash", ["borghash.pyx"]),
 ]
 setup(
     name='borghash',
-    packages=['borghash'],
+#    packages=['borghash'],
     ext_modules=cythonize(ext_modules))
